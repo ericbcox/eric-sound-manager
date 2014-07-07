6101e30b286876e12466206a40bb8da2f5695edd
ericbcox <eric_cox@baylor.edu>
2014-07-06 21:09:00 -0500

    Update posts_controller.rb

--- sound/app/controllers/posts_controller.rb
+++ sound/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   def new
   end
 
@@ -7,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.order(sort_column + ' ' + sort_direction)
   end
 
   def create
@@ -16,9 +17,44 @@ class PostsController < ApplicationController
     @post.save
     redirect_to @post
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to posts_path
+  end
+  
+  def delete
+    @post = Post.find(params[:id]).destroy
+    redirect_to @post
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
+  def sort_column
+    Post.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
 
   private
   def post_params
     params.require(:post).permit(:name, :format, :length, :size)
   end
+  
 end
